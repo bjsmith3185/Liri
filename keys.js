c7904b9bb7be74c0cc1b2e9176791b0f20d5da95
bjsmith3185 <bjsmith3185@hotmail.com>
2018-08-31 21:03:34 -0400

    got spotify, movie and concert api's working

---
+++ keys.js
@@ -0,0 +1,16 @@
+console.log('this is loaded');
+
+// exports.spotify = {
+//   id: process.env.SPOTIFY_ID,
+//   secret: process.env.SPOTIFY_SECRET
+// };
+
+exports.spotify = {
+  id: "d1dd394176784365a4b2991023279225",
+  secret: "b67b278082324aa8929cd6cacf246d72"
+};
+
+
+// Client ID d1dd394176784365a4b2991023279225
+// Client Secret b67b278082324aa8929cd6cacf246d72 
+
